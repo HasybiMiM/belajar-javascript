8a77bcb5282acb16d05b7a51624d0f21ad8714d6
githubna-HasybiMiM <hasybimim@gmail.com>
2024-01-30 08:50:34 +0700

    Json bang

--- javascript-dom/objek.js
+++ javascript-dom/objek.js
@@ -6,7 +6,17 @@ siswa.namaLengkap = function(){
     return this.namaDepan + " " + this.namaBelakang;
 };
 
+// object JSON
+var siswa2 = {
+    namaDepan : "Hasybi",
+    namaBelakang : "mim",
+    alamat : "Bandung",
+    namaLengkap : function(){
+        return this.namaDepan + " " + this.namaBelakang;
+    }
+}
 alert("Nama : " + siswa.namaLengkap());
+alert("Nama : " + siswa2.namaLengkap());
 // function namaLengkap(){
 
 // }
